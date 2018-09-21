c033a49ff1745cfc83a7c9e689fb0c3de5b95663
DanielDan95 <ddan@kth.se>
2018-09-21 06:11:15 +0200

    start

--- src/Start.java
+++ src/Start.java
@@ -1,5 +1,16 @@
+import java.net.*;
+
+
 public class Start {
-    public static void main(String args []) {
-        System.out.println("hej");
+    public static void main(String args []) throws Exception {
+        int port = 5000;
+        try {
+            ServerSocket socket = new ServerSocket(port);
+
+        }
+        catch (Exception E) {
+            System.exit(1);
+        }
+
     }
 }
